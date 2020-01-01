52abfc70109eb5404b6e7e2a14e9e576bcbd1cfb
Hansuk <flavono123@gmail.com>
2020-01-01 15:21:00 +0900

    initial commit
    - default vue project as ts
    - add keen ui
    - main CTA 'Love u'
    
    Signed-off-by: Hansuk <flavono123@gmail.com>

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Hello msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Hello from "./components/Hello.vue";
+
+@Component({
+  components: {
+    Hello
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+
+html {
+    font-size: 100%;
+}
+</style>
---
+++ client/src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Hi! {{ wavingHand }} I love you</p>
+    <UiButton class="ui-button-group">Me, too!</UiButton>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { UiButton } from 'keen-ui';
+import 'keen-ui/dist/keen-ui.css';
+
+@Component({
+  components: {
+    UiButton
+  }
+})
+export default class Hello extends Vue {
+  data() {
+    return {
+      wavingHand: '👋'
+    }
+  }
+}
+</script>

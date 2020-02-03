d29514310a8edcf2a5bbe56409026dbbc9412b6a
Hansuk <flavono123@gmail.com>
2020-02-03 16:52:24 +0900

    vue create flavono123-client
    Signed-off-by: Hansuk <flavono123@gmail.com>

---
+++ flavono123-client/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <UiButton>Me, too!</UiButton>
+  </div>
+</template>
+
+<script>
+import { UiButton } from "keen-ui";
+import 'keen-ui/dist/keen-ui.css';
+export default {
+  name: "HelloWorld",
+  components: {
+    UiButton
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
